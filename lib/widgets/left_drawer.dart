a14828708af445bd11b2762387e4e6bb5315d7d8
ardhika23 <ardhika@gmail.com>
2023-11-16 18:12:44 +0700

    Finished Tutorial 8

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/screens/menu.dart';
 import 'package:shopping_list/screens/shoplist_form.dart';
+import 'package:shopping_list/screens/list_product.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -27,7 +28,6 @@ class LeftDrawer extends StatelessWidget {
                 ),
                 Padding(padding: EdgeInsets.all(10)),
                 Text("Write all your shopping needs here!",
-                    // Adding text style with center alignment, font size 15, white color, and regular weight
                   style: TextStyle(
                     fontSize: 15,
                     color: Colors.white,
@@ -40,8 +40,8 @@ class LeftDrawer extends StatelessWidget {
           ),
           ListTile(
             leading: const Icon(Icons.home_outlined),
-            title: const Text('Home Page'),
-            // Redirection to MyHomePage
+            title: const Text('Homa Page'),
+            // redirect to MyHomePage
             onTap: () {
               Navigator.pushReplacement(
                   context,
@@ -53,12 +53,22 @@ class LeftDrawer extends StatelessWidget {
           ListTile(
             leading: const Icon(Icons.add_shopping_cart),
             title: const Text('Add Product'),
-            // Redirection to ShopFormPage
+            // redirect to ShopFormPage
             onTap: () {
-              // Routing to ShopFormPage here, after ShopFormPage has been created.
               Navigator.pushReplacement(
-                context, 
-                MaterialPageRoute(builder: (context) => const ShopFormPage())
+                  context,
+                  MaterialPageRoute(builder: (context) => ShopFormPage())
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.shopping_basket),
+            title: const Text('Product List'),
+            onTap: () {
+              // Route menu to product page
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ProductPage()),
               );
             },
           ),
